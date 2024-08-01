d82c9a186152988465bbd88ae66bc144691565d3
samelin <same.lin@qq.com>
2024-08-01 12:23:16 +0800

    系统目录隔符

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,2 +1,4 @@
+import path from 'path';
+
 // 使用正则表达式，全局替换所有的反斜杠为正斜杠
-export const formatPath = (str: string) => str.replace(/\//g, '\\');
+export const formatPath = (str: string) => str.replace(/[/\\]+/g, path.sep);
